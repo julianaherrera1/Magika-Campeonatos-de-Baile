6471c349c169995a70166f59b0ee27ca28ba8608
Juliana Herrera <julianaherrera522@gmail.com>
2025-04-01 11:28:01 -0500

    feat: Diseño de la primera seccion en el header - Agregado video de fondo en el header - Incluidos dos titulos principales - Agregado boton CTA - Ajustes de estilo para mejorar la presentacion - Diseño Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
     content: ["./dist/**/*.html"], // Cambiado para que detecte archivos en dist
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'headings': ['"Rajdhani"', 'sans-serif'],    // Titulares - urbano delgado pero potente
+          'subheadings': ['"Exo 2"', 'sans-serif'],    // Subtítulos - futurista elegante
+          'body': ['"Karla"', 'sans-serif'],            // Texto general - legibilidad profesional
+        },
+      colors: {
+          'magika-purple': '#373634',
+          'magika-pink': '#c3a655',
+          'magika-yellow': '#f1eff2',
+      },
+      },
     },
     plugins: [],
   }
